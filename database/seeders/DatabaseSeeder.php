cf10c3c141bbcd565f9b8a542da07b456757f831
pebrianto-3322 <pebrianto@students.amikom.ac.id>
2026-05-26 13:43:05 +0700

    feat: tambah partner seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -90,5 +90,18 @@ class DatabaseSeeder extends Seeder
             'stock'       => 30,
             'poster_path' => 'assets/workshop.png',
         ]);
+
+        // === PARTNER ===
+        $partners = [
+            ['name' => 'Google', 'logo_url' => 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png'],
+            ['name' => 'About You-1975', 'logo_url' => 'https://i.scdn.co/image/ab67616d0000b2731f44db452a68e229650a302c'], 
+        ];
+
+        foreach ($partners as $partner) {
+            \App\Models\Partner::firstOrCreate(
+                ['name' => $partner['name']],
+                ['logo_url' => $partner['logo_url']]
+            );
+        }
     }
 }
